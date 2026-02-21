0105dfb74d3e1a6a52c9920e370ddf923a7d72d4
Richard Shaji Mekkaden <richardshajimekkaden@Richards-MacBook-Air.local>
2026-02-21 22:01:16 +0530

    Learned Docker

--- REACT/TRYING REACT ROUTES/react-router-demo/src/main.jsx
+++ REACT/TRYING REACT ROUTES/react-router-demo/src/main.jsx
@@ -1,14 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-document.body.style.margin = "0";
-document.body.style.backgroundColor = "#121212";
-document.body.style.color = "white";
-document.body.style.fontFamily = "sans-serif";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
